4701510e0d5c4ef280bfc22ccc084fa38d78f896
A706\1dam <manucarrera34@gmail.com>
2024-09-26 17:24:11 +0200

    Bateria ejercicios 2 - Ejercicio 4

--- Ejercicios/src/main/java/org/example/Main.java
+++ Ejercicios/src/main/java/org/example/Main.java
@@ -19,7 +19,10 @@ public class Main {
 //        Ejercicio2 ejercicio2 = new Ejercicio2();
 //        ejercicio2.ejercicio2();
 
-        Ejercicio3 ejercicio3 = new Ejercicio3();
-        ejercicio3.ejercicio3();
+//        Ejercicio3 ejercicio3 = new Ejercicio3();
+//        ejercicio3.ejercicio3();
+
+        Ejercicio4 ejercicio4 = new Ejercicio4();
+        ejercicio4.ejercicio4();
     }
 }
