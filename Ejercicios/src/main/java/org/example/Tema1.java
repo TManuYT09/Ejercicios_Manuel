479ca6d4d1177daf25310e0736628a9b675a4d95
A706\1dam <manucarrera34@gmail.com>
2024-10-10 16:47:04 +0200

    Eliminación enunciado

--- Ejercicios/src/main/java/org/example/Tema1.java
+++ Ejercicios/src/main/java/org/example/Tema1.java
@@ -1,9 +1,5 @@
 package org.example;
 
-//Realizar un programa que pida un número al usuario, que deberá introducirlo usando el teclado.
-// El algoritmo debe identificar si el número introducido se trata de un número positivo (> 0)
-// o un número negativo (< 0) y controlar el caso particular del número 0, que es natural.
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
