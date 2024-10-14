28da677a34e9f17aaf0a559de7cbdc635013c50d
A706\1dam <manucarrera34@gmail.com>
2024-10-14 20:56:03 +0200

    Actividad 1 de los 2 modos

---
+++ Ejercicios/src/main/java/org/example/Excepciones.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Excepciones {
+    public void excepciones(){
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Introduce el divisor: ");
+        String numero = teclado.next();
+
+        int dividendo = 0;
+
+        System.out.println("Introduce el dividendo: ");
+
+        if (teclado.hasNextInt()){
+            dividendo = teclado.nextInt();
+        }else {
+            System.out.println("El formato no es numérico...");
+        }
+
+        int num_entero = 0;
+        int division = 0;
+
+        try{
+            num_entero = Integer.parseInt(numero);
+            division = dividendo / num_entero;
+        }catch (NumberFormatException e1){
+            System.out.println("No has escrito un número... " + e1.getMessage());
+        }catch (ArithmeticException e2){
+            System.out.println("El denominador es 0. División errónea. "+ e2.getMessage());
+        }finally {
+            if (num_entero != 0){
+                System.out.println("El resultado es "+ division);
+            }
+        }
+    }
+}
--- Ejercicios/src/main/java/org/example/Tema2.java
+++ Ejercicios/src/main/java/org/example/Tema2.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.time.LocalDateTime;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Tema2 {
@@ -80,6 +82,94 @@ public class Tema2 {
         }
         if (intentos == 0){
             System.out.println("Lo siento, no has acertado ninguna palabra");
+        }
+    }
+    public void actividad1(){
+        Scanner entrada = new Scanner(System.in);
+        LocalDateTime hoy = LocalDateTime.now();
+        int anyo_actual = hoy.getYear();
+
+        System.out.println("Elige el modo del programa");
+        int modo = 0;
+        boolean comp = true;
+        while (comp){
+            try{
+                modo = entrada.nextInt();
+                comp = false;
+            }catch (InputMismatchException err){
+                System.out.println("Introducción erronea del modo debido a caracteres no deseados: "+ err);
+                entrada.nextLine();
+            }
+        }
+        int anyo = 1800;
+
+        switch (modo){
+            case 1:
+                System.out.println("Introduzca tu año de nacimiento");
+                String ano = entrada.next();
+
+                try{
+                    anyo = Integer.parseInt(ano);
+                }catch (NumberFormatException e){
+                    System.out.println("Has introducido un formato erróneo. No es un número.");
+                }
+
+                if (anyo<1900||anyo>anyo_actual){
+                    System.out.println("El año introducido no es corrector");
+                }else if (anyo >= 1900 && anyo <= 1927){
+                        System.out.println("Tu generazión no fue bautizada");
+                }else if (anyo <= 1944){
+                    System.out.println("Tu generación es Silent");
+                }else if (anyo >= 1945 && anyo <= 1964){
+                    System.out.println("Tu generación es Baby Boomers");
+                }else if (anyo >= 1965 && anyo <= 1981){
+                    System.out.println("Tu generación es X");
+                }else if (anyo >= 1982 && anyo <= 1994){
+                    System.out.println("Tu generación es Y/Milennial");
+                }else if (anyo >= 1995 && anyo <= anyo_actual){
+                    System.out.println("Tu generación es Z/Centennials");
+                }else {
+                    System.out.println("No eres de ninguna generación");
+                }
+
+                break;
+            case 2:
+                System.out.println("Introduzca su edad:");
+                int edad = 0;
+
+                if (entrada.hasNextInt()){
+                    edad = entrada.nextInt();
+                }else {
+                    System.out.println("La edad introducida no tiene un formato válido");
+                }
+
+                if (edad < 0){
+                  System.out.println("La edad introducida no es válida.");
+                }else {
+                     anyo = anyo_actual - edad;
+                    System.out.println("Naciste en el "+ anyo);
+                }
+
+                if (anyo<1900||anyo>anyo_actual){
+                    System.out.println("El año introducido no es corrector");
+                }else if (anyo >= 1900 && anyo <= 1927){
+                    System.out.println("Tu generazión no fue bautizada");
+                }else if (anyo <= 1944){
+                    System.out.println("Tu generación es Silent");
+                }else if (anyo >= 1945 && anyo <= 1964){
+                    System.out.println("Tu generación es Baby Boomers");
+                }else if (anyo >= 1965 && anyo <= 1981){
+                    System.out.println("Tu generación es X");
+                }else if (anyo >= 1982 && anyo <= 1994){
+                    System.out.println("Tu generación es Y/Milennial");
+                }else if (anyo >= 1995 && anyo <= anyo_actual){
+                    System.out.println("Tu generación es Z/Centennials");
+                }else {
+                    System.out.println("No eres de ninguna generación");
+                }
+
+                break;
+            default:System.out.println("El modo "+ modo +" no existe.");
         }
     }
 }
